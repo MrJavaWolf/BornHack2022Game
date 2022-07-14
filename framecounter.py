2f43225b897ac913a4989ff302309024d6d7e223
Jens Larsen <mr.javawolf@gmail.com>
2022-07-14 23:56:11 +0200

    Added: Print stats to frame counter, making it easy to print its stats. Improved: Formatting

--- framecounter.py
+++ framecounter.py
@@ -2,10 +2,13 @@ import displayio
 from adafruit_display_text import label
 import terminalio
 import time
+import json
+from collections import OrderedDict
 
 SHOW_FRAME_COUNT = True
 SHOW_FRAMES_PER_SECOND = True
 FRAMES_PER_SECOND_UPDATE_SPEED = 0.1
+
 
 class FrameCounter:
     """A frame counter to calculate Frames Per Second and total number of frames"""
@@ -13,25 +16,30 @@ class FrameCounter:
     total_frames: int = 0
     """The total number of frames rendered"""
 
-    frames_per_second_counter:float = 0
-    frames_per_second:float = 0
+    frames_per_second: float = 0
     """Number of frames rendered a second"""
 
-    start_time = 0
+    frames_per_second_counter: float = 0
+    """Used to calculate the FPS"""
+
+    frames_per_second_time = 0
     """Used to calculate the FPS"""
 
     def __init__(self):
         self.sprite = displayio.Group(scale=1, x=1, y=3)
-       
-        # Total frames
-        self.total_frames_label = label.Label(terminalio.FONT, text=str(self.total_frames), color=0xFF0000)
-        self.sprite.append(self.total_frames_label)
-        
-        # FPS
-        self.start_time = time.monotonic()
-        self.frames_per_second_label = label.Label(terminalio.FONT, y=10, text=str(int(self.frames_per_second)), color=0xFF0000)
-        self.sprite.append(self.frames_per_second_label)
 
+        # Total frames
+        self.total_frames_label = label.Label(
+            terminalio.FONT, text=str(self.total_frames), color=0xFF0000
+        )
+        self.sprite.append(self.total_frames_label)
+
+        # FPS
+        self.frames_per_second_time = time.monotonic()
+        self.frames_per_second_label = label.Label(
+            terminalio.FONT, y=10, text=str(int(self.frames_per_second)), color=0xFF0000
+        )
+        self.sprite.append(self.frames_per_second_label)
 
     def loop(self):
         # Total frames
@@ -40,9 +48,25 @@ class FrameCounter:
 
         # Frames per second
         self.frames_per_second_counter += 1
-        current_time = time.monotonic() 
-        if (current_time - self.start_time) > FRAMES_PER_SECOND_UPDATE_SPEED:
-            self.frames_per_second = self.frames_per_second_counter / (current_time - self.start_time)
+        current_time = time.monotonic()
+        if (
+            current_time - self.frames_per_second_time
+        ) > FRAMES_PER_SECOND_UPDATE_SPEED:
+            self.frames_per_second = self.frames_per_second_counter / (
+                current_time - self.frames_per_second_time
+            )
             self.frames_per_second_label.text = str(int(self.frames_per_second))
             self.frames_per_second_counter = 0
-            self.start_time = current_time
+            self.frames_per_second_time = current_time
+
+    def print_state(self):
+        print(
+            json.dumps(
+                OrderedDict(
+                    {
+                        "total_frames": self.total_frames,
+                        "frames_per_second": self.frames_per_second,
+                    }
+                )
+            )
+        )
