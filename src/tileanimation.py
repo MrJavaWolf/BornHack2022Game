01072eb76aec7b817d32cabff21d6675206c9a21
Jens Larsen <mr.javawolf@gmail.com>
2022-07-17 13:54:59 +0200

    Added: More comments and documentation

--- src/tileanimation.py
+++ src/tileanimation.py
@@ -1,12 +1,21 @@
-import displayio
 from gametime import GameTime
 from displayio import TileGrid
 
 
 class TileAnimation:
+    """Animates a single tile on a tile grid"""
+
+    tile_grid: TileGrid
+    """The tile grid to animate"""
+
+    tile_grid_index: int = 0
+    """Which tile in the tile grid to animate"""
 
     frames = []
-    speed: float = 0.1
+    """A list of tile indexs which will be looped through. Looping through this list is the animation"""
+
+    speed: float = 0.5
+    """The animation speed in seconds. How long each frame in the animation will be shown"""
 
     __current_frame = -1
     __last_frame_change = 0
@@ -18,26 +27,38 @@ class TileAnimation:
         speed: float,
         tile_grid_index=0,
     ):
-        self.frames = frames
+        """
+        Creates a animation for a single tile in the tile grid 
+        Args:
+            tile_grid: The tile grid to animate
+            frames: A list of tile indexs which will be looped through. Looping through this list is the animation
+            speed: The speed in seconds of how fast the frames should change
+            tile_grid_index: Which tile in the tile grid to animate
+        """
         self.tile_grid = tile_grid
+        self.frames = frames
         self.speed = speed
         self.tile_grid_index = tile_grid_index
 
     def reset(self):
+        """Restart the animation"""
         self.__current_frame = -1
         self.__last_frame_change = 0
 
     def loop(self, game_time: GameTime):
-        if self.__current_frame == -1:
+        # The very start of the animation
+        if self.__current_frame == -1: 
             self.__current_frame = 0
-            self.__update_frame(game_time)
+            self.__animate_tile_grid(game_time)
         
+        # Checks if it is time to change the frame
         if self.__last_frame_change + self.speed < game_time.total_time:
             self.__current_frame += 1
             if self.__current_frame >= len(self.frames):
                 self.__current_frame = 0
-            self.__update_frame(game_time)
+            self.__animate_tile_grid(game_time)
 
-    def __update_frame(self, game_time: GameTime):        
+    def __animate_tile_grid(self, game_time: GameTime):
+        """Changes the tile on the tile grid"""    
         self.tile_grid[self.tile_grid_index] = self.frames[self.__current_frame]
         self.__last_frame_change = game_time.total_time
--- src/tilegridloader.py
+++ src/tilegridloader.py
@@ -8,15 +8,30 @@ import displayio
 
 TRANSPARENT_COLOR = 0xFF00FF
 
+
 def import_tile_grid(
     image_path,
     tile_pixel_width,
     tile_pixel_height,
-    height=1,
     width=1,
+    height=1,
     default_tile=0,
     transparent_color=TRANSPARENT_COLOR,
 ):
+    """
+    Import a sprite sheet into a tile grid.
+    This import can make specific colors transparent.
+    For a better understanding of how tile grids work, please see the links at the top of the file
+
+    Args:
+        image_path: The path to the sprite sheet image (.bmp) to load
+        tile_pixel_width: The width of a tile in pixel
+        tile_pixel_heigh: The height of a tile in pixel
+        width: The number of tiles the created tile grid will have in the x direction
+        height: The number of tiles the created tile grid will have in the y direction
+        default_tile: Default tile index to show in the created tile grid
+        transparent_color: Make a specific color in your sprite sheet transparent. Usefull for characters and props. Set to None to not make any colors transparent
+    """
     # Loads the image
     bitmap, palette = adafruit_imageload.load(image_path)
 
